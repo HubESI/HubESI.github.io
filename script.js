f508c21cf9bd90e5bb8d64d0040f51be4efa6187
Nassim Boulechfar <in_boulechfar@esi.dz>
2021-08-15 22:03:51 +0100

    Feat adapt to api changes

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const API_URL = "https://esihub.herokuapp.com";
 const url_params = new URLSearchParams(window.location.search);
 if(url_params.has("code")){
     const alert = document.getElementById("join-with-gh");
-    fetch(`${API_URL}/check?code=${url_params.get("code")}`)
+    fetch(`${API_URL}/invite?code=${url_params.get("code")}`)
         .then((response) => response.json())
         .then((data) => {
+            if(!data.hasOwnProperty("success")) throw Error(JSON.stringify(data));
             if(data.success) window.location.replace("https://github.com/HubESI");
             else{
                 alert.style.color = "red";
-                alert.innerHTML = data.description;
+                alert.innerHTML = data.github_user + ". " + data.message;
             }
         })
         .catch((error) => {
